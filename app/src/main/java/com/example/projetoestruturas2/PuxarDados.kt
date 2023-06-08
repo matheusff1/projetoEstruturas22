1acc4b069663c7a7307077fb9c2ef0be1fc4b0d3
Henrique C <hcortezi10@hotmail.com>
2023-06-08 17:11:08 -0300

    firebase e inicio de puxar dados

--- app/src/main/java/com/example/projetoestruturas2/PuxarDados.kt
+++ app/src/main/java/com/example/projetoestruturas2/PuxarDados.kt
@@ -1,8 +1,9 @@
 package com.example.projetoestruturas2
 
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class PuxarDados {
-
+    val db = Firebase.firestore
 
 }
